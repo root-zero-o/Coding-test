fc31a9058ad8c8a757416e9dd54811a498f34176
GeunYeong Kim <97326130+root-zero-o@users.noreply.github.com>
2024-08-29 14:48:12 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 212 ms, Memory: 31120 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -6,7 +6,10 @@ N = int(input())
 a = 0
 b = 1
 
-for i in range(2, N+1):
-    a, b = b % 1000000007, (a+b) % 1000000007
+if N == 0 :
+    print(a)
+else:
+    for i in range(2, N+1):
+        a, b = b % 1000000007, (a+b) % 1000000007
  
-print(b)
+    print(b)
