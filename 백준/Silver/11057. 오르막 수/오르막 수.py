11b2b6a1cff3d4daffb40e121c44ef2d8365c2b9
GeunYeong Kim <97326130+root-zero-o@users.noreply.github.com>
2024-09-03 16:16:31 +0900

    [Silver I] Title: 오르막 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -2,13 +2,11 @@ N = int(input())
 
 dp = [[1 for _ in range(10)] for _ in range(N+1)]
 
-if N > 1 :
-    for i in range(2, N+1) :
-        for j in range(10):
-            if j == 0 :
-                dp[i][j] = 1
-            else :
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+for i in range(1, N+1) :
+    for j in range(10):
+        if j == 0 :
+            dp[i][j] = 1
+        else :
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-print(sum(dp[-1]) % 10007)
-
+print(dp[-1][-1] % 10007)
