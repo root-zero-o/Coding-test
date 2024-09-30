aab0212168b24835467a0b6a0c5a8f108fe5b470
Geunyeong Kim <97326130+root-zero-o@users.noreply.github.com>
2024-09-30 10:13:46 +0900

    [Silver II] Title: DFS와 BFS, Time: 60 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,44 +1,40 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
+# 인접 리스트
 N, M, V = map(int, input().split())
-
-# 1) 인접 리스트 만들기
 lst = [[] for _ in range(N+1)]
 for i in range(M):
     start, end = map(int, input().split())
     lst[start].append(end)
     lst[end].append(start)
-
-# 2) 작은 노드부터 방문 -> sort
 for i in range(N+1):
     lst[i].sort()
 
-
-# 2) 방문한 노드 저장
-visited = [0] * (N+1)
-
-# 3) dfs
-def dfs(start):
-    visited[start] = 1
-    print(start, end=" ")
-    for i in lst[start]:
-        if visited[i] == 0:
+# dfs
+visited_d = [0] * (N+1)
+def dfs(v):
+    visited_d[v] = 1
+    print(v, end=" ")
+    for i in lst[v]:
+        if not visited_d[i] :
             dfs(i)
 
-# 4) bfs
-bfsVisited = [0] * (N+1)
+# bfs
+visited_b = [0] * (N+1)
+def bfs(v):
+    queue = deque([v])
+    visited_b[v] = 1
 
-def bfs(start):
-    queue = deque([start])
-    bfsVisited[start] = 1
     while queue:
-        v = queue.popleft()
-        print(v, end=' ')
-        for i in lst[v]:
-            if not bfsVisited[i]:
+        num = queue.popleft()
+        print(num, end=" ")
+        for i in lst[num]:
+            if not visited_b[i]:
                 queue.append(i)
-                bfsVisited[i] = 1
+                visited_b[i] = 1
 
 dfs(V)
-print('')
+print()
 bfs(V)
